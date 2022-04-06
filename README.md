15b938225827b1c29e15fbbcb00b5b8a0c956836
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-06 08:40:48 +0530

    add

---
+++ README.md
@@ -0,0 +1,67 @@
+# Memory Game
+
+This is a simple Memory Game build by me. The game has features like showing of cards on click and once the cards mismatch then the cards will again be hidden within 0.5s. This showing and hiding of cards will occur by a smooth transition. If the cards matches then the cards will remain visible.
+
+## Tech Stack
+
+**Client:** HTML, CSS, JavaScript.
+
+## About the Project
+
+The game has features like showing of cards on click and once the cards mismatch then the cards will again be hidden within 0.5s. This showing and hiding of cards will occur by a smooth transition. If the cards matches then the cards will remain visible.
+
+- Each time the player mismatch a pair of cards his/her number of 'Failed Attempts' will decrease by 1
+
+- Every time a pair of cards are matched the score will increase by 1
+
+- Once the user reaches the maximum score an alert function is fired showing the player that he has own.
+
+- The initially the color of 'Failed Attempts' and 'Score' is black but as the player Failed Attempts gets non zero its color changes to '#f45725' and similarly for score the color changes to 'gold'.
+
+This project's interface uses DOM Manipulation to create a toggle effect on the hamburger button. This project also contains SVG amination
+
+```bash
+    <div class="burger" onclick="hamburger(event)">
+        <div id="bur1"></div>
+        <div id="bur2"></div>
+        <div id="bur3"></div>
+    </div>
+```
+
+```bash
+    #burger3 {
+        transform-origin: left;
+        transform: rotate(-45deg);
+    }
+```
+
+```bash
+    #bur3 {
+        transition: all 0.5s;
+    }
+```
+
+```bash
+    document.getElementById("bur3").id = "burger3";
+```
+
+and a glass interface to enhance player experience.
+
+```bash
+    background: linear-gradient( to right bottom, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.3));
+    backdrop-filter: blur(2rem);
+    box-shadow: 10px 10px 100px rgba(161, 161, 161, 0.5);
+```
+
+The project also contains a **Copy To Clipboard** to copy the developer's email.
+
+```bash
+    var copyText = document.getElementById("emailInp");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+```
+
+## Author
+
+- [@TheDa21crj](https://github.com/TheDa21crj)
---
+++ script.js
@@ -0,0 +1,119 @@
+let s = null;
+let sb = 0,
+    fa = 0,
+    hB = 0;
+
+const colors = ["batman1", "spiderman1", "superman1", "joker1", "tiger1"];
+
+const cards = [...document.querySelectorAll(".card")];
+for (let color of colors) {
+    const cardAIndex = parseInt(Math.random() * cards.length);
+    const cardA = cards[cardAIndex];
+    cards.splice(cardAIndex, 1);
+    cardA.className += ` ${color}`;
+    cardA.setAttribute("data-color", color);
+
+    const cardBIndex = parseInt(Math.random() * cards.length);
+    const cardB = cards[cardBIndex];
+    cards.splice(cardBIndex, 1);
+
+    cardB.className += ` ${color}`;
+    cardB.setAttribute("data-color", color);
+}
+
+function mom(event) {
+    if (event.target === s || event.target.className.includes("done")) {
+        return;
+    }
+    event.target.className = event.target.className.replace("c-hideq", "").trim();
+    event.target.className += " done";
+    if (!s) {
+        s = event.target;
+    } else if (s) {
+        if (
+            s.getAttribute("data-color") !== event.target.getAttribute("data-color")
+        ) {
+            setTimeout(() => {
+                s.className = s.className.replace("done", "").trim() + " c-hideq";
+                event.target.className =
+                    event.target.className.replace("done", "").trim() + " c-hideq";
+                s = null;
+                fa = fa + 1;
+                document.getElementsByTagName("body").cursor = "pointer";
+                document.getElementById("ScoreBoard").innerHTML = "-" + fa;
+                document.getElementById("failedAttempet").style.color = "#f45725";
+            }, 500);
+        } else {
+            sb = sb + 1;
+            document.getElementById("ScoreBoard2").innerHTML = sb;
+            document.getElementById("scoreDiv").style.color = "gold";
+            s = null;
+            if (sb == 5) {
+                setTimeout(() => {
+                    document.getElementById("alert").style.opacity = "1";
+                    document.getElementById("alert").style.visibility = "visible";
+                    document.getElementById("alert").innerHTML =
+                        "You Have Won in " + fa + " failed attempts";
+                    setTimeout(() => {
+                        document.getElementById("alert").style.opacity = "0";
+                        document.getElementById("alert").style.visibility = "hidden";
+                    }, 500);
+                });
+                document.getElementById("ScoreBoard").innerHTML = fa;
+                document.getElementById("ScoreBoard2").innerHTML = sb;
+            }
+        }
+    }
+}
+
+function hamburger(event) {
+    hB++;
+    if (hB % 2 == 0) {
+        document.getElementById("svgDiv").style.opacity = "0";
+        document.getElementById("svgDiv").style.visibility = "hidden";
+        document.getElementById("svgDiv").style.transform = "translateX(-500px)";
+        document.getElementById("burger1").id = "bur1";
+        document.getElementById("emailVal").style.opacity = "0";
+        document.getElementById("emailVal").style.visibility = "hidden";
+        document.getElementById("burger2").id = "bur2";
+        document.getElementById("burger3").id = "bur3";
+        document.getElementById("raisedH").id = "raisedHand";
+        document.getElementById("onH").id = "onHand";
+    } else {
+        document.getElementById("svgDiv").style.opacity = "1";
+        document.getElementById("svgDiv").style.visibility = "visible";
+        document.getElementById("svgDiv").style.transform = "translateX(0px)";
+        document.getElementById("bur1").id = "burger1";
+        document.getElementById("bur2").id = "burger2";
+        document.getElementById("bur3").id = "burger3";
+        document.getElementById("raisedHand").id = "raisedH";
+        document.getElementById("onHand").id = "onH";
+    }
+}
+
+function emailF(event) {
+    setTimeout(() => {
+        document.getElementById("emailVal").style.opacity = "1";
+        document.getElementById("emailVal").style.visibility = "visible";
+        setTimeout(() => {
+            document.getElementById("emailVal").style.opacity = "0";
+            document.getElementById("emailVal").style.visibility = "hidden";
+        }, 3000);
+    });
+}
+
+function copyClip(event) {
+    var copyText = document.getElementById("emailInp");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    setTimeout(() => {
+        document.getElementById("alert").style.opacity = "1";
+        document.getElementById("alert").style.visibility = "visible";
+        document.getElementById("alert").innerHTML = "Copied to clipboard";
+        setTimeout(() => {
+            document.getElementById("alert").style.opacity = "0";
+            document.getElementById("alert").style.visibility = "hidden";
+        }, 500);
+    });
+}
