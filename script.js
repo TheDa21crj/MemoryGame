1bb37a319b6e8e1ab6b8bd00e866b16903bef217
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-06 08:10:58 +0530

    working

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let clickedCard = null;
+let NoClick = false;
+let score = 0;
 
 function cardClicked(e) {
     let target = e.target;
 
-    if (target === clickedCard || target.className.includes("done")) {
+    if (NoClick || target === clickedCard || target.className.includes("done")) {
         return;
     }
 
@@ -17,6 +19,7 @@ function cardClicked(e) {
             clickedCard.getAttribute("data-color") !==
             target.getAttribute("data-color")
         ) {
+            NoClick = true;
             console.log("not same");
             setTimeout(() => {
                 clickedCard.className =
@@ -24,9 +27,14 @@ function cardClicked(e) {
                 target.className =
                     target.className.replace("done", "").trim() + " colorHidden";
                 clickedCard = null;
+                NoClick = false;
             }, 500);
         } else {
+            score++;
             clickedCard = null;
+            if (score === 8) {
+                console.log("Score = 8");
+            }
         }
     }
 }
