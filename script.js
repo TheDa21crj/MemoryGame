4aa74b22bbc0bfe7f5413a38932971ec4e8819e0
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-04-06 07:38:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let clickedCard = null;
+
 function cardClicked(e) {
     let target = e.target;
     target.className = target.className.replace("colorHidden", "").trim();
+
+    if (!clickedCard) {
+        clickedCard = target;
+    } else if (clickedCard) {}
 }
